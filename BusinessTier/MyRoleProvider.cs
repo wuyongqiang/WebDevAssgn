2a464efcc2da3d26da19cf3c7069528109cf7da1
Grant Wu <wuyongqiang@tsinghua.org.cn>
2011-04-26 21:40:35 +0800

    add myroleprovider, change 'change my details'

---
+++ BusinessTier/MyRoleProvider.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Security;
+using System.Configuration;
+using System.Web.Configuration;
+using System.Configuration.Provider;
+using System.Data.SqlClient;
+using System.Data;
+
+
+
+/// <summary>
+/// Summary description for MyRoleProvider
+/// </summary>
+
+namespace RestaurantApp
+{
+    public class MyRoleProvider : System.Web.Security.RoleProvider
+    {
+        public MyRoleProvider()
+        {
+
+        }
+
+        //
+        // A helper function to retrieve config values from the configuration file.
+        //
+        private string GetConfigValue(string configValue, string defaultValue)
+        {
+            if (String.IsNullOrEmpty(configValue))
+                return defaultValue;
+
+            return configValue;
+        }
+
+
+
+        public override void Initialize(string name, System.Collections.Specialized.NameValueCollection config)
+        {
+            base.Initialize(name, config);
+            pApplicationName = GetConfigValue(config["applicationName"],
+                                          System.Web.Hosting.HostingEnvironment.ApplicationVirtualPath);
+
+            ConnectionStringSettings ConnectionStringSettings =
+            ConfigurationManager.ConnectionStrings[config["connectionStringName"]];
+
+            if (ConnectionStringSettings == null || ConnectionStringSettings.ConnectionString.Trim() == "")
+            {
+                throw new ProviderException("Connection string cannot be blank.");
+            }
+
+            connectionString = ConnectionStringSettings.ConnectionString;
+
+
+        }
+
+        private string pApplicationName;
+
+        private string connectionString;
+
+        public override string ApplicationName
+        {
+            get
+            {
+                return pApplicationName;
+            }
+            set
+            {
+                pApplicationName = value;
+            }
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            PersistData.MyRoleProcedures.CreateRole(roleName, connectionString,  ApplicationName);
+
+        }
+        
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            return PersistData.MyRoleProcedures.DeleteRole(roleName, throwOnPopulatedRole,connectionString,  ApplicationName);
+        }
+
+        
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            return PersistData.MyRoleProcedures.FindUsersInRole(roleName, usernameToMatch, connectionString,  ApplicationName);
+        }
+
+
+        
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            PersistData.MyRoleProcedures.AddUsersToRoles(usernames, roleNames, connectionString,  ApplicationName);
+        }
+
+
+        
+        public override string[] GetAllRoles()
+        {
+            return PersistData.MyRoleProcedures.GetAllRoles(connectionString,  ApplicationName);
+        }
+
+        
+        public override string[] GetRolesForUser(string username)
+        {
+            return PersistData.MyRoleProcedures.GetRolesForUser(username, connectionString,  ApplicationName);
+        }
+
+        
+        public override string[] GetUsersInRole(string roleName)
+        {
+            return PersistData.MyRoleProcedures.GetUsersInRole(roleName, connectionString,  ApplicationName);
+
+        }
+
+
+        
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            return PersistData.MyRoleProcedures.IsUserInRole(username, roleName, connectionString,  ApplicationName);
+        }
+
+
+        
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            PersistData.MyRoleProcedures.RemoveUsersFromRoles(usernames, roleNames, connectionString,  ApplicationName);
+        }
+
+
+        
+        public override bool RoleExists(string roleName)
+        {
+            return PersistData.MyRoleProcedures.RoleExists(roleName,connectionString,  ApplicationName);
+        }
+    }
+}
---
+++ PersistData/MyRoleProcedures.cs
@@ -0,0 +1,474 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+
+
+namespace PersistData
+{
+    public class MyRoleProcedures
+    {
+        public static void CreateRole(string roleName, string connectionString, string ApplicationName)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = conn;
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.CommandText = "[dbo].[aspnet_Roles_CreateRole]";
+
+            cmd.Parameters.Add(new SqlParameter("@ApplicationName", SqlDbType.NVarChar, 256,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, ApplicationName));
+
+            cmd.Parameters.Add(new SqlParameter("@RoleName", SqlDbType.NVarChar, 256,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, roleName));
+
+            cmd.Parameters.Add(new SqlParameter("@return_value", SqlDbType.Int, 4,
+                ParameterDirection.ReturnValue, false, 4, 0, "", DataRowVersion.Current, -1));
+
+            conn.Open();
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            if ((int)cmd.Parameters[2].Value != 0)
+                throw new Exception("aspnet_Roles_CreateRole not executed correctedly");
+        }
+
+        //  EXECUTE @RC = [n7682905].[dbo].[aspnet_Roles_DeleteRole] 
+        // @ApplicationName
+        //,@RoleName
+        //,@DeleteOnlyIfRoleIsEmpty
+        public static bool DeleteRole(string roleName, bool throwOnPopulatedRole, string connectionString, string ApplicationName)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = conn;
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.CommandText = "[dbo].[aspnet_Roles_DeleteRole]";
+
+            cmd.Parameters.Add(new SqlParameter("@ApplicationName", SqlDbType.NVarChar, 256,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, ApplicationName));
+
+            cmd.Parameters.Add(new SqlParameter("@RoleName", SqlDbType.NVarChar, 256,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, roleName));
+
+            cmd.Parameters.Add(new SqlParameter("@DeleteOnlyIfRoleIsEmpty", SqlDbType.Bit, 1,
+                ParameterDirection.Input, false, 1, 0, "", DataRowVersion.Current, throwOnPopulatedRole));
+
+            cmd.Parameters.Add(new SqlParameter("@return_value", SqlDbType.Int, 4,
+                ParameterDirection.ReturnValue, false, 4, 0, "", DataRowVersion.Current, -1));
+
+            conn.Open();
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            if ((int)cmd.Parameters[3].Value != 0)
+                return false;
+            else
+                return true;
+        }
+
+        //  EXECUTE @RC = [n7682905].[dbo].[aspnet_UsersInRoles_FindUsersInRole] 
+        // @ApplicationName
+        //,@RoleName
+        //,@UserNameToMatch
+        public static string[] FindUsersInRole(string roleName, string usernameToMatch, string connectionString, string ApplicationName)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = conn;
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.CommandText = "[dbo].[aspnet_UsersInRoles_FindUsersInRole]";
+
+            cmd.Parameters.Add(new SqlParameter("@ApplicationName", SqlDbType.NVarChar, 256,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, ApplicationName));
+
+            cmd.Parameters.Add(new SqlParameter("@RoleName", SqlDbType.NVarChar, 256,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, roleName));
+
+            cmd.Parameters.Add(new SqlParameter("@UserNameToMatch", SqlDbType.NVarChar, 256,
+                ParameterDirection.Input, false, 1, 0, "", DataRowVersion.Current, usernameToMatch));
+
+            cmd.Parameters.Add(new SqlParameter("@return_value", SqlDbType.Int, 4,
+                ParameterDirection.ReturnValue, false, 4, 0, "", DataRowVersion.Current, -1));
+
+            conn.Open();
+
+
+
+            SqlDataReader sr = cmd.ExecuteReader();
+
+
+
+            if ((int)cmd.Parameters[3].Value != 0)
+                throw new Exception("aspnet_UsersInRoles_FindUsersInRole not executed correctedly");
+
+
+            List<string> userList = new List<string>();
+
+            while (!sr.Read())
+            {
+                userList.Add(sr.GetString(0));
+            }
+
+
+            conn.Close();
+
+            string[] sA = new string[userList.Count];
+
+            for (int i = 0; i < userList.Count; i++)
+                sA[0] = userList[0];
+
+            return sA;
+        }
+
+        //  EXECUTE @RC = [n7682905].[dbo].[aspnet_UsersInRoles_AddUsersToRoles] 
+        // @ApplicationName
+        //,@UserNames
+        //,@RoleNames
+        //,@CurrentTimeUtc
+        public static void AddUsersToRoles(string[] usernames, string[] roleNames, string connectionString, string ApplicationName)
+        {
+            string UserNames = "";
+            string RoleNames = "";
+            foreach (string s in usernames)
+            {
+                UserNames += s + ",";
+            }
+            int len = UserNames.Length;
+            if ((len > 0) && (UserNames[len - 1] == ','))
+                UserNames = UserNames.Substring(0, len - 1);
+            foreach (string s in roleNames)
+            {
+                RoleNames += s + ",";
+            }
+
+            len = RoleNames.Length;
+            if ((len > 0) && (RoleNames[len - 1] == ','))
+                RoleNames = RoleNames.Substring(0, len - 1);
+
+            //
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = conn;
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.CommandText = "[dbo].[aspnet_UsersInRoles_AddUsersToRoles]";
+
+            cmd.Parameters.Add(new SqlParameter("@ApplicationName", SqlDbType.NVarChar, 256,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, ApplicationName));
+
+            cmd.Parameters.Add(new SqlParameter("@UserNames", SqlDbType.NVarChar, 4000,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, UserNames));
+
+            cmd.Parameters.Add(new SqlParameter("@RoleNames", SqlDbType.NVarChar, 4000,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, RoleNames));
+
+
+            cmd.Parameters.Add(new SqlParameter("@CurrentTimeUtc", SqlDbType.DateTime, 8,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, DateTime.UtcNow));
+
+            cmd.Parameters.Add(new SqlParameter("@return_value", SqlDbType.Int, 4,
+                ParameterDirection.ReturnValue, false, 4, 0, "", DataRowVersion.Current, -1));
+
+            conn.Open();
+
+            cmd.ExecuteNonQuery();
+
+
+            conn.Close();
+
+            if ((int)cmd.Parameters[4].Value != 0)
+                throw new Exception("aspnet_Roles_GetAllRoles not executed correctly");
+        }
+
+        // EXECUTE @RC = [n7682905].[dbo].[aspnet_Roles_GetAllRoles] 
+        //@ApplicationName
+        public static string[] GetAllRoles(string connectionString, string ApplicationName)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = conn;
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.CommandText = "[dbo].[aspnet_Roles_GetAllRoles]";
+
+            cmd.Parameters.Add(new SqlParameter("@ApplicationName", SqlDbType.NVarChar, 256,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, ApplicationName));
+
+
+
+            cmd.Parameters.Add(new SqlParameter("@return_value", SqlDbType.Int, 4,
+                ParameterDirection.ReturnValue, false, 4, 0, "", DataRowVersion.Current, -1));
+
+            conn.Open();
+
+            SqlDataReader sr = cmd.ExecuteReader();
+
+            List<string> userList = new List<string>();
+
+            while (sr.Read())
+            {
+                userList.Add(sr.GetString(0));
+
+            }
+
+
+            conn.Close();
+
+            string[] sA = new string[userList.Count];
+
+            for (int i = 0; i < userList.Count; i++)
+                sA[i] = userList[i];
+
+            return sA;
+        }
+
+        //  EXECUTE @RC = [n7682905].[dbo].[aspnet_UsersInRoles_GetRolesForUser] 
+        // @ApplicationName
+        //,@UserName
+        public static string[] GetRolesForUser(string username, string connectionString, string ApplicationName)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = conn;
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.CommandText = "[dbo].[aspnet_UsersInRoles_GetRolesForUser]";
+
+            cmd.Parameters.Add(new SqlParameter("@ApplicationName", SqlDbType.NVarChar, 256,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, ApplicationName));
+
+
+            cmd.Parameters.Add(new SqlParameter("@UserName", SqlDbType.NVarChar, 256,
+               ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, username));
+
+
+            cmd.Parameters.Add(new SqlParameter("@return_value", SqlDbType.Int, 4,
+                ParameterDirection.ReturnValue, false, 4, 0, "", DataRowVersion.Current, -1));
+
+            conn.Open();
+
+            SqlDataReader sr = cmd.ExecuteReader();
+
+            List<string> userList = new List<string>();
+
+            while (sr.Read())
+            {
+                userList.Add(sr.GetString(0));
+
+            }
+
+            conn.Close();
+
+            string[] sA = new string[userList.Count];
+
+            for (int i = 0; i < userList.Count; i++)
+                sA[i] = userList[i];
+
+            return sA;
+
+        }
+
+        //  EXECUTE @RC = [n7682905].[dbo].[aspnet_UsersInRoles_GetUsersInRoles] 
+        // @ApplicationName
+        //,@RoleName
+        public static string[] GetUsersInRole(string roleName, string connectionString, string ApplicationName)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = conn;
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.CommandText = "[dbo].[aspnet_UsersInRoles_GetUsersInRoles]";
+
+            cmd.Parameters.Add(new SqlParameter("@ApplicationName", SqlDbType.NVarChar, 256,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, ApplicationName));
+
+
+            cmd.Parameters.Add(new SqlParameter("@RoleName", SqlDbType.NVarChar, 256,
+               ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, roleName));
+
+
+
+            cmd.Parameters.Add(new SqlParameter("@return_value", SqlDbType.Int, 4,
+                ParameterDirection.ReturnValue, false, 4, 0, "", DataRowVersion.Current, -1));
+
+            conn.Open();
+
+            SqlDataReader sr = cmd.ExecuteReader();
+
+            List<string> userList = new List<string>();
+
+            while (sr.Read())
+            {
+                userList.Add(sr.GetString(0));
+
+            }
+
+
+            conn.Close();
+
+            string[] sA = new string[userList.Count];
+
+            for (int i = 0; i < userList.Count; i++)
+                sA[i] = userList[i];
+
+            return sA;
+        }
+
+        //EXECUTE @RC = [n7682905].[dbo].[aspnet_UsersInRoles_IsUserInRole] 
+        //   @ApplicationName
+        //  ,@UserName
+        //  ,@RoleName
+        public static bool IsUserInRole(string username, string roleName, string connectionString, string ApplicationName)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = conn;
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.CommandText = "[dbo].[aspnet_UsersInRoles_IsUserInRole]";
+
+            cmd.Parameters.Add(new SqlParameter("@ApplicationName", SqlDbType.NVarChar, 256,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, ApplicationName));
+
+
+            cmd.Parameters.Add(new SqlParameter("@UserName", SqlDbType.NVarChar, 256,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, username));
+
+            cmd.Parameters.Add(new SqlParameter("@RoleName", SqlDbType.NVarChar, 256,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, roleName));
+
+            cmd.Parameters.Add(new SqlParameter("@return_value", SqlDbType.Int, 4,
+                ParameterDirection.ReturnValue, false, 4, 0, "", DataRowVersion.Current, -1));
+
+            conn.Open();
+
+            cmd.ExecuteNonQuery();
+
+            bool rlt = ((int)cmd.Parameters[3].Value == 1);
+
+
+            return rlt;
+        }
+
+        //  EXECUTE @RC = [n7682905].[dbo].[aspnet_UsersInRoles_RemoveUsersFromRoles] 
+        // @ApplicationName
+        //,@UserNames
+        //,@RoleNames
+        public static void RemoveUsersFromRoles(string[] usernames, string[] roleNames, string connectionString, string ApplicationName)
+        {
+            string UserNames = "";
+            string RoleNames = "";
+            foreach (string s in usernames)
+            {
+                UserNames += s + ",";
+            }
+            int len = UserNames.Length;
+            if ((len > 0) && (UserNames[len - 1] == ','))
+                UserNames = UserNames.Substring(0, len - 1);
+            foreach (string s in roleNames)
+            {
+                RoleNames += s + ",";
+            }
+
+            len = RoleNames.Length;
+            if ((len > 0) && (RoleNames[len - 1] == ','))
+                RoleNames = RoleNames.Substring(0, len - 1);
+
+            //
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = conn;
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.CommandText = "[dbo].[aspnet_UsersInRoles_RemoveUsersFromRoles]";
+
+            cmd.Parameters.Add(new SqlParameter("@ApplicationName", SqlDbType.NVarChar, 256,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, ApplicationName));
+
+            cmd.Parameters.Add(new SqlParameter("@UserNames", SqlDbType.NVarChar, 4000,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, UserNames));
+
+            cmd.Parameters.Add(new SqlParameter("@RoleNames", SqlDbType.NVarChar, 4000,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, RoleNames));
+
+            cmd.Parameters.Add(new SqlParameter("@return_value", SqlDbType.Int, 4,
+                ParameterDirection.ReturnValue, false, 4, 0, "", DataRowVersion.Current, -1));
+
+            conn.Open();
+
+            cmd.ExecuteNonQuery();
+
+
+            conn.Close();
+
+            if ((int)cmd.Parameters[3].Value != 0)
+                throw new Exception("aspnet_UsersInRoles_RemoveUsersFromRoles not executed correctly");
+
+
+        }
+
+        //EXECUTE @RC = [n7682905].[dbo].[aspnet_Roles_RoleExists] 
+        //   @ApplicationName='Homework8'
+        //  ,@RoleName='wuyq1'
+        public static bool RoleExists(string roleName, string connectionString, string ApplicationName)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = conn;
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.CommandText = "[dbo].[aspnet_Roles_RoleExists]";
+
+            cmd.Parameters.Add(new SqlParameter("@ApplicationName", SqlDbType.NVarChar, 256,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, ApplicationName));
+
+            cmd.Parameters.Add(new SqlParameter("@RoleName", SqlDbType.NVarChar, 256,
+                ParameterDirection.Input, false, 30, 0, "", DataRowVersion.Current, roleName));
+
+            cmd.Parameters.Add(new SqlParameter("@return_value", SqlDbType.Int, 4,
+                ParameterDirection.ReturnValue, false, 4, 0, "", DataRowVersion.Current, -1));
+
+            conn.Open();
+
+            cmd.ExecuteNonQuery();
+
+            bool rlt = ((int)cmd.Parameters[2].Value == 1);
+
+
+            return rlt;
+        }
+
+    }
+}
